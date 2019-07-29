22e78b71f50f53caeecb17c1d4560741e7f0499a
Diego Savoia <diego.savoia@mail.polimi.it>
2019-07-29 16:13:31 +0200

    Written method "delrel"

--- main.c
+++ main.c
@@ -27,6 +27,7 @@ typedef Entities * entities_pointer;
 entities_pointer addent(char const input[], entities_pointer firstEntity);
 entities_pointer delent(char const input[], entities_pointer firstEntity);
 entities_pointer addrel(char const input[], entities_pointer firstEntity);
+entities_pointer delrel(char const input[], entities_pointer firstEntity);
 
 
 /**
@@ -49,6 +50,8 @@ int main() {
             entitiesList = delent(input, entitiesList);
         else if(strstr(input, "addrel") == input)           //If the command begins with "addrel"
             entitiesList = addrel(input, entitiesList);
+        else if(strstr(input, "delrel") == input)           //If the command begins with "delrel"
+            entitiesList = delrel(input, entitiesList);
 
 
 
@@ -365,5 +368,127 @@ entities_pointer addrel(char const input[], entities_pointer firstEntity) {
         }
     }
 
+    return firstEntity;
+}
+
+
+entities_pointer delrel(char const input[], entities_pointer firstEntity) {
+    int i = 7;                              //It's the beginning index of the new origin id received
+    int originIDLength = 0;
+
+    while (input[i] != ' ') {               //To get the size of the new origin id
+        originIDLength++;
+        i++;
+    }
+
+    char originID[originIDLength + 1];      //It will contain the new origin id, plus the '\0'
+
+    i = 7;
+    int j = 0;
+
+    while (input[i] != ' ') {               //It initializes originID with the origin id received
+        originID[j] = input[i];
+        i++;
+        j++;
+    }
+
+    originID[j] = '\0';                     //Adds the '\0'
+
+
+    i++;                                    //Index i now points to the first " of the dest id received in input
+    int destIDStart = i;
+    int destIDLength = 0;
+    while (input[i] != ' ') {               //To get the size of the new dest id
+        destIDLength++;
+        i++;
+    }
+
+    char destID[destIDLength + 1];          //It will contain the new dest id, plus the '\0'
+
+    i = destIDStart;
+    j = 0;
+
+    while (input[i] != ' ') {               //It initializes destID with the dest id received
+        destID[j] = input[i];
+        i++;
+        j++;
+    }
+
+    destID[j] = '\0';                       //Adds the '\0'
+
+
+    i++;                                    //Index i now points to the first " of the rel id received in input
+    int relIDStart = i;
+    int relIDLength = 0;
+    while (input[i] != '\0') {              //To get the size of the new rel id
+        relIDLength++;
+        i++;
+    }
+
+    char relID[relIDLength + 1];            //It will contain the new rel id, plus the '\0'
+
+    i = relIDStart;
+    j = 0;
+
+    while (input[i] != '\0') {              //It initializes relID with the rel id received
+        relID[j] = input[i];
+        i++;
+        j++;
+    }
+
+    relID[j] = '\0';                        //Adds the '\0'
+
+
+    //It looks for the entity node with the same name as "destID"
+
+    entities_pointer ptr = firstEntity;
+    while (ptr != NULL && strcmp(ptr->name, destID) != 0) {
+        ptr = ptr->next;
+    }
+
+    //If that entity is not present, it exits.
+    //If that entity is present it checks if it has relID in its relations list. If not, it exits.
+    //If relID is present, it checks if the originID of that relation is the same as the originID in input. If not, it exits.
+    //If it is the same, it remove that origin in that relation.
+
+    if(ptr != NULL) {
+        relations_pointer relPointer = ptr->relations;
+        relations_pointer relPointerPrec = ptr->relations;
+        int counterRel = 0;
+        while(relPointer != NULL && strcmp(relPointer->name, relID) != 0) {
+            relPointerPrec->next = relPointer;
+            relPointer = relPointer->next;
+            counterRel++;
+        }
+
+        if(relPointer != NULL) {
+            origins_pointer originPointer = relPointer->origins;
+            origins_pointer originPointerPrec = relPointer->origins;
+            int countOrigins = 0;
+            while(originPointer != NULL && strcmp(originPointer->name, originID) != 0) {
+                originPointerPrec->next = originPointer;
+                originPointer = originPointer->next;
+                countOrigins++;
+            }
+
+            if(originPointer != NULL && countOrigins == 0) {
+                relPointer->origins = originPointer->next;
+                free(originPointer);
+            }
+            else if(originPointer != NULL) {
+                originPointerPrec->next = originPointer->next;
+                free(originPointer);
+            }
+
+            if(relPointer->origins == NULL && counterRel == 0) {
+                ptr->relations = relPointer->next;
+                free(relPointer);
+            }
+            else if(relPointer->origins == NULL) {
+                relPointerPrec->next = relPointer->next;
+                free(relPointer);
+            }
+        }
+    }
     return firstEntity;
 }
